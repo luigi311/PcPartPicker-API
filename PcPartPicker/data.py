094e5122ddc590d22a5825fadc3a5e56eca4f39c
Simon <simjenner3@gmail.com>
2017-10-04 22:05:24 +0100

    Initial push

---
+++ PcPartPicker/data.py
@@ -0,0 +1,76 @@
+lookup = {
+    "cpu": {
+        1: "Name",
+        2: "Speed",
+        3: "Cores",
+        4: "TDP",
+        5: "Ratings",
+        6: "Price"
+    },
+    "cpu-cooler": {
+        1: "Name",
+        2: "Fan RPM",
+        3: "Noise Level",
+        4: "Ratings",
+        5: "Price"
+    },
+    "motherboard": {
+        1: "Name",
+        2: "Socket",
+        3: "Form Factor",
+        4: "RAM Slots",
+        5: "Max RAM",
+        6: "Ratings",
+        7: "Price"
+    },
+    "memory": {
+        1: "Name",
+        2: "Speed",
+        3: "Type",
+        4: "CAS",
+        5: "Modules",
+        6: "Size",
+        7: "Price/GB",
+        8: "Ratings",
+        9: "Price"
+    },
+    "internal-hard-drive": {
+        1: "Name",
+        2: "Series",
+        3: "Form",
+        4: "Type",
+        5: "Capacity",
+        6: "Cache",
+        7: "Price/GB",
+        8: "Ratings",
+        9: "Price"
+    },
+    "video-card": {
+        1: "Name",
+        2: "Series",
+        3: "Chipset",
+        4: "Memory",
+        5: "Core Clock",
+        6: "Ratings",
+        7: "Price"
+    },
+    "power-supply": {
+        1: "Name",
+        2: "Series",
+        3: "Form",
+        4: "Efficiency",
+        5: "Watts",
+        6: "Modular",
+        7: "Ratings",
+        8: "Price"
+    },
+    "case": {
+        1: "Name",
+        2: "Type",
+        3: "Ext. 5 1/4",
+        4: "Int. 3 1/2",
+        5: "Power Supply",
+        6: "Ratings",
+        7: "Price"
+    }
+}
