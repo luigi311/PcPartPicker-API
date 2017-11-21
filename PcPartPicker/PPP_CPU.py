b915ebbdca12a3e49c12c8a18b69258d6577e772
Luis Garcia <luigi311.lg@gmail.com>
2017-11-20 20:58:57 -0700

    Cpu support with socket information

--- PcPartPicker/PPP_CPU.py
+++ PcPartPicker/PPP_CPU.py
@@ -3,52 +3,59 @@ from ._PPP_data import lookup
 from bs4 import BeautifulSoup
 from requests import get
 
-base_url = "https://pcpartpicker.com/products/{}/fetch?page={}"
+base_url = "https://pcpartpicker.com/products/cpu/fetch?k={}&page={}"
+sockets={27:"AM1", 3:"AM3" ,4:"AM3+",6:"AM3/AM2+", 
+         33:"AM4", 8:"BGA413", 10:"BGA559", 
+         25:"BGA1023", 32:"C32", 20:"FM1", 
+         23:"FM2", 26:"FM2+", 31:"G34", 12:"LGA771",
+         13:"LGA775", 24:"LGA1150", 30:"LGA1151", 
+         14:"LGA1155", 15:"LGA1156", 37:"LGA1356",
+         16:"LGA1366", 21:"LGA2011", 28:"LGA2011-3",
+         35:"LGA2066", 18:"PGA988", 36:"TR4" }
 
-
-def _get_page(part_type, page_num, return_pagenum=False):
+def _get_page(socket, page_num, return_pagenum=False):
     """
-    part_type = part type, for example, "cpu"
+    socket = part type, for example, "cpu"
     page_num = page number, for example, 1
     return_pagenum = whether to return the number of pages or the html soup
     """
-    r = get(base_url.format(part_type, page_num))
+    r = get(base_url.format(socket, page_num))
     parsed = jsonloads(r.content.decode("utf-8"))
     if return_pagenum:
         return parsed["result"]["paging_data"]["page_blocks"][-1]["page"]
     return BeautifulSoup(parsed["result"]["html"], "html.parser")
 
-def get_total_pages(part_type):
-    return _get_page(part_type, 1, True)
+def get_total_pages(socket):
+    return _get_page(socket, 1, True)
 
-def get_part(part_type, single_page=False):
+def get_part(socket, single_page=False):
     """
-    part_type = Any item in data.lookup
+    socket = Any item in data.lookup
     single_page = Page number. False by default (False means all pages)
     """
-    if part_type not in lookup:
-        raise ValueError("part_type not valid")
 
     if single_page:
         start_page_num, total_pages = single_page, single_page
     else:
-        start_page_num, total_pages = 1, _get_page(part_type, 1, True)
+        start_page_num, total_pages = 1, _get_page(socket, 1, True)
 
     parsed_html = []
     for page_num in range(start_page_num, total_pages+1):
-        soup = _get_page(part_type, page_num)
+        soup = _get_page(socket, page_num)
         for row in soup.findAll("tr"):
             row_elements = {}
             for count, value in enumerate(row):
                 text = value.get_text().strip()
                 try:
-                    row_elements[lookup[part_type][count]] = text
+                    row_elements[lookup[socket][count]] = text
                 except KeyError:
                     if count == 1:
                         row_elements["name"] = text
+                        row_elements["socket"] = sockets[socket]
                     elif count == len(row)-2:
                         row_elements["price"] = text
                     elif count == len(row)-3:
                         row_elements["ratings"] = text.replace("(", "").replace(")", "")
             parsed_html.append(row_elements)
+            
     return parsed_html
--- components.py
+++ components.py
@@ -1,20 +1,10 @@
 from PcPartPicker import PPP_API
 import json
 
-components = ["cpu"]
-sockets={"AM1":27, "AM3":3 ,"AM3+":4,"AM3/AM2+":6, 
-         "AM4":33, "BGA413":8, "BGA559":10, 
-         "BGA1023":25, "C32":32, "FM1":20, 
-         "FM2":23, "FM2+":26, "G34":31, "LGA771":12,
-         "LGA775":13, "LGA1150":24, "LGA1151":30, 
-         "LGA1155":14, "LGA1156":15, "LGA1356":37,
-         "LGA1366":16, "LGA2011":21, "LGA2011-3":28,
-         "LGA2066":35, "PGA988":18, "TR4":36 }
-sockets_value = sockets.values()
 
-for i in range(len(components)):
-    for j in sockets_value:
-        info = PPP_API.get_part(components[i],j)
+components = ["motherboard","memory"]
+for comp in components:
+    cpu_info = PPP_API.get_part(comp)
 
-        with open(components[i]+".json", "w") as outfile:
-            json.dump(info, outfile)
+    with open(comp+".json", "w") as outfile:
+        json.dump(cpu_info, outfile)
