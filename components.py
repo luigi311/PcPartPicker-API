1d1c1bfc0c0e5c245b08667a2079e65c07345e0b
Luis Garcia <luigi311.lg@gmail.com>
2017-11-28 17:08:23 -0700

    Fix cpu calling wrong function. Add script to combine cpus to 1 list

--- components.py
+++ components.py
@@ -10,14 +10,17 @@ sockets={"AM1":27, "AM3":3 ,"AM3+":4,"AM3/AM2+":6,
          "LGA2066":35, "PGA988":18, "TR4":36 }
 
 components = ["motherboard","memory"]
+print("Working on")
 for comp in components:
+    print(comp)
     comp_info = PPP_API.get_part(comp)
 
     with open(comp+".json", "w") as outfile:
         json.dump(comp_info, outfile)
 
+print("Cpus")
 for key,value in sockets.items():
-    cpu_info = PPP_API.get_part(value)
+    cpu_info = PPP_CPU.get_part(value)
 
     with open("cpu.json", "a+") as outfile:
         json.dump(cpu_info, outfile)
